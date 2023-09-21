185dce226f045c207189a2f8c13aa4340383b126
VictoriaAdebiyi <victoriaadebiyi.p@gmail.com>
2023-09-21 20:19:31 +0100

    Flask-calculator

--- app.py
+++ app.py
@@ -1,35 +1,47 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, request, render_template, redirect, url_for
+#Creating Flask instance
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+#Defining a route for our home page ('/')
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
+    @app.route('/result/', methods=['POST'])
 def result():
-    Input1 = float(request.form['Input1'])
-    Input2 = float(request.form['Input2'])
-    result = Input1 + Input2
-    operation = request.form['operation']
+    return render_template('index.html')
+
+         Input1 = float(request.form['Input1'])
+         Input2 = float(request.form['Input2'])
+         result = Input1 + Input2
+         operation = request.form['operation']
+
+#Calculating the result based on selected operation
     if operation == 'addition':
         result = Input1 + Input2
     elif operation == 'subtraction':
-            result = Input1 - Input2    
+        result = Input1 - Input2    
     elif operation == 'division':
         if Input2 != 0:
             result = Input1 / Input2
     elif operation == 'multiplication':
-            result = Input1 * Input2           
-        else:
-            result = "Error: Division by zero"
+        result = Input1 * Input2           
+    else:
+        result = "Invalid operation"
+        operation = "Invalid operation"
 
-    return render_template('result.html', result=result)
+     return render_template(
+        'index.html',
+        Input1=Input1,
+        Input2=Input2,
+        operation=operation,
+        result=result 
+     )
+    
 
-
-
-
-
+#Run the flask application
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
+    
